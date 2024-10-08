0377bf9aa82873feed8d1d456a0f143944f6032f
agabin <agabin@appdev.yearup.org>
2024-10-08 10:33:24 -0400

    Completed Exercise 2 - Roll The Dice

---
+++ RollTheDice/src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Dice {
+
+//Create a method named "roll" to generate random numbers between 1 & 6
+
+    public int roll(){
+        int randomNumber = (int) (Math.random() * 6) + 1;
+        return randomNumber;
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ RollTheDice/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,47 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+//Create an instance of Dice named "dice"
+        Dice dice = new Dice();
+
+//Identify variables
+        int roll1 = 0;
+        int roll2;
+        int counterFor2 = 0;
+        int counterFor4 = 0;
+        int counterFor6 = 0;
+        int counterFor7 = 0;
+
+//Create a loop that runs 100x, call the roll method two times
+        for (int i = 0; i < 100; i++) {
+            roll1 = dice.roll();
+            roll2 = dice.roll();
+            int sum = roll1 + roll2;
+
+//Display the value of each roll
+            System.out.println("Roll " + (i+1) + ": " + roll1 + " - " + roll2 + " Sum: " + sum);
+
+//Implement counters for rolls resulting in 2, 4, 6, & 7
+            if(sum == 2) {
+                counterFor2++;
+            }
+            if(sum == 4){
+                counterFor4++;
+            }
+            if(sum == 6){
+                counterFor6++;
+            }
+            if(sum == 7){
+                counterFor7++;
+            }
+        }
+//Display counters
+        System.out.println("Counter for 2: " + counterFor2);
+        System.out.println("Counter for 4: " + counterFor4);
+        System.out.println("Counter for 6: " + counterFor6);
+        System.out.println("Counter for 7: " + counterFor7);
+    }
+}
